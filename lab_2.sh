be7fdc835d9022a3bdf28d38b3394bdc4089923a
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-19 21:39:43 +0400

    Update and rename lab2.sh to lab_2.sh

--- lab_2.sh
+++ lab_2.sh
@@ -1,51 +1,51 @@
-#region 1
-cat > file1 # Ստեղծում է ֆայլ file1. և սպասում է ստեղնաշարի մուտքագրմանը մինչև Ctrl+D մուտքագրվի: Մուտքագրված տեքստը կգրվի file1-ում:
-cat file1 # Ցուցադրում է file1-ի բովանդակությունը:
-cat < file1 # Ցուցադրում է file1-ի բովանդակությունը:
-#endregion
+#exersize 1
 
-#region 2
+cat > file1 
+cat file1 
+cat < file1 
+
+#exersize 2
+
 cat > file1.txt
 cat > file2.txt
 cat > file3.txt
 cat file1.txt file2.txt file3.txt > Final.txt
-#endregion
 
-#region 3
+#exersize 3
+
 ls ./*.txt | wc -l
-#endregion
 
-#region 4
+#exersize 4 
+
 cat file1.txt file2.txt file3.txt | sort > sorted.txt
-#endregion
 
-#region 5
+#exersize 5
+
 ls -S ./ | head -n 15
-#endregion
 
-#region 6
+#exersiz3 6
+
 ls ./ > ls.txt
-#endregion
 
-#region 7
+#exersize 7
+
 echo "secret text" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
 echo "frperg grkg" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
-#endregion
 
-#region 8
+#exersize 8
+
 echo "Student’s home directory is {home_dir}." > home.txt
 sed 's/{home_dir}/\/home\/student/' home.txt > home_modified.txt
-#endregion
 
-#region 9
+#exersize 9
+
 echo "Line 1" > file4.txt
 echo "Line 2" >> file4.txt
 echo "Line 3" >> file4.txt
 echo "Line 4" >> file4.txt
 echo "Line 5" >> file4.txt
 sed -n '2p;4p' file4.txt
-#endregion
 
-#region 10
+#exersize 10
+
 sed -i '2d;4d' file4_modified.txt
-#endregion
