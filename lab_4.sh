eff7e95989380ce5b623b2c8a05f37b5593c582c
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-19 21:43:18 +0400

    Update and rename lab4.sh to lab_4.sh

--- lab_4.sh
+++ lab_4.sh
@@ -1,52 +1,42 @@
 #!/bin/bash
 
-#region 1
+#exersize 1
 touch file.txt
 chmod 744 file.txt
-#endregion
 
-#region 2
+#exersize 2
 chmod a+x file.txt
-#endregion
 
-#region 3
+#exersize 3
 chmod go-x file.txt
-#endregion
 
-#region 4
+#exersize 4
 chmod 600 file.txt
-#endregion
 
-#region 5
+#exersize 5
 mkdir dir
 chmod 666 dir
 touch dir/file.txt
-#endregion
 
-#region 6
+#exersize 6
 touch dir/file1.txt dir/file2.txt
 chmod 766 dir/*
-#endregion
 
-#region 7
+#exersize 7
 umask 027
-#endregion
 
-#region 8
+#exersize 8
 umask 077
 umask 022
-#endregion
 
-#region 9
+#exersize 9
 echo 'ls /home/student' > script.sh
 chmod 741 script.sh
 ./script.sh
 chmod +x script.sh
-#endregion
 
-#region 10
+#exersize 10
 mkdir ~/bin
 mv script.sh ~/bin
 echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
 source ~/.bashrc
-#endregion
